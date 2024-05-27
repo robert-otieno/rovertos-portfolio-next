f2be71befb0d6024aa4ff41160b54e09b527c574
Robert Otieno <robertotieno.or@gmail.com>
2024-05-27 14:15:31 +0300

    minor homepage update

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,10 @@
 import { motion } from "framer-motion";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function Home({ isFirstMount }) {
+  const router = useRouter();
+
   const homepage = {
     initial: {
       opacity: 0,
@@ -27,12 +30,23 @@ export default function Home({ isFirstMount }) {
         {/*Main Content*/}
         <div className='home container'>
           <div className='home__wrapper'>
-            <motion.section animate='animate' variants={homepage} className='home__section content-box'>
+            <motion.section animate='animate' variants={homepage} className='home__section content-box flex flex-row'>
+              {/* <img src='../assets/robert.jpg' className=' w-48 max-w-xs rounded-lg shadow-2xl' /> */}
               <div className='home__content'>
                 <h1 className='home__description'>
-                  Hi, I am Robert Otieno <br className='md:leading-[3rem]' />
-                  <span className="text-base font-light">Systems Librarian | Full Stack Developer,</span>
+                  Hi! I am Robert, <br className='md:leading-[3rem]' />
+                  <span className='text-lg font-light tracking-wider'>A Systems Librarian & Full Stack Developer based in Nairobi.</span>
                 </h1>
+                <button className='btn btn-primary mb-6' onClick={() => router.push("https://sites.google.com/view/robertotieno/about")}>
+                  View my profile{" "}
+                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='h-6 w-6'>
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z'
+                    />
+                  </svg>
+                </button>
                 <h2 className='home__tags'>
                   {/* <span className='home__tag hidden md:block'>Freelancer</span> */}
                   <span className='home__tag'>ReactJS</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        // primary: "#a52924",
+        secondaryCustom: "#F7AB0A",
+      },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      }
+        sans: ["Inter", "sans-serif"],
+      },
     },
   },
-  plugins: [require('tailwind-scrollbar-hide')],
-}
+  plugins: [require("tailwind-scrollbar-hide"), require('daisyui'),],
+  daisyui: {
+    themes: ["bumblebee",],
+  },
+};
